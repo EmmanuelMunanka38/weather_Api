5e606deb935c6b8a50db643b4610bbf0c6efba36
Emmanuel Mnanka Samo <emmanuelmunanka38@gmail.com>
2026-07-13 17:13:02 +0300

    refactor: simplify main entry point to load config and run on port 8080

--- main.cc
+++ main.cc
@@ -1,12 +1,12 @@
 #include <drogon/drogon.h>
-int main() {
-    //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0", 5555);
-    //Load config file
-    drogon::app().loadConfigFile("../config.json");
-    drogon::app().loadConfigFile("../config.yaml");
-    //Run HTTP framework,the method will block in the internal event loop
-    drogon::app().run();
+
+int main()
+{
+    drogon::app()
+        .setLogLevel(trantor::Logger::kDebug)
+        .loadConfigFile("../config.json")
+        .addListener("0.0.0.0", 8080)
+        .setThreadNum(4)
+        .run();
     return 0;
 }
-
